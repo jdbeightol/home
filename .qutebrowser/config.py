3c7f30415d48985ff3f05c3e75a502050d358526
Jeremy Beightol <jdbeightol@gmail.com>
2021-02-26 02:38:15 -0500

    added comments to config.py

--- .qutebrowser/config.py
+++ .qutebrowser/config.py
@@ -1,14 +1,41 @@
-c.bindings.commands = {"command": {}, "insert": {"<Escape>": "leave-mode ;; jseval -q document.activeElement.blur()"}, "normal": {"J": "tab-prev", "K": "tab-next", "j": "scroll-px 0 200", "k": "scroll-px 0 -200"}}
+# these custom bindings achieve the following:
+# - ensure that we leave any text boxes when running mode-leave for
+#   predictability
+# - to help with scrolling
+# - switch J and K tab direction to be "left/right" rather than "up/down"
+c.bindings.commands = {
+  "command": {},
+  "insert": {
+    "<Escape>": "mode-leave ;; jseval -q document.activeElement.blur()"
+  },
+  "normal": {
+    "J": "tab-prev",
+    "K": "tab-next",
+    "j": "scroll-px 0 200",
+    "k": "scroll-px 0 -200"
+  }
+}
+
+# this is our editor command; it may not be portable since kitty isn't portable
 c.editor.command = ["/usr/local/bin/kitty", "vim", "-f", "{file}", "-c", "normal {line}G{column0}l"]
 
-# TODO: Experimenting with these to see if insert behavior becomes more predictable.
-#c.input.insert_mode.auto_load = True
+# these input mode settings help make qutebrowser more predictable
 c.input.insert_mode.auto_enter = False
 c.input.insert_mode.auto_leave = False
 c.input.insert_mode.auto_load = False
 c.input.insert_mode.plugins = False
 
+# load tabs in the foreground; we can use ;r for background loading
+c.tabs.background = False
+
+# try to make scrolling more visually tolerable since we changed scrolling 
+# distances above
 c.scrolling.smooth = True
-c.tabs.background = True
+
+# load autoconfig here to ensure local config overrides shared config --e.g.,
+# to use leave-mode rather than mode-leave on v2.0+
 config.load_autoconfig()
+
+# apply the theme after loading autoconfig
 config.source('theme.py')
+
