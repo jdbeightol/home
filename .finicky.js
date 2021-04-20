584f93590727d54a30d1d2e9b5fe29370dc4b6c1
Jeremy Beightol <jbeightol@digitalocean.com>
2021-04-20 02:12:41 -0400

    updated .finicky to handle meet some links, default qutebrowser.

--- .finicky.js
+++ .finicky.js
@@ -2,17 +2,15 @@
 // Learn more about configuration options: https://github.com/johnste/finicky/wiki/Configuration
 
 module.exports = {
-  defaultBrowser: "Safari",
-/*
+  defaultBrowser: "Qutebrowser",
   handlers: [
     {
-      // Open google.com and *.google.com urls in Google Chrome
       match: [
-        "google.com*", // match google.com urls
-        finicky.matchDomains(/.*\.google.com/) // use helper function to match on domain only
+        "calendar.google.com*",
+        "meet.google.com*",
+        "do.co/bbzd"
       ],
       browser: "Google Chrome"
     }
   ]
-*/
 }
