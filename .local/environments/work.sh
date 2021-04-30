02d94dff114a4ae51f5113db60f3d7af4ee765f1
Jeremy Beightol <jbeightol@digitalocean.com>
2021-04-30 17:06:15 -0400

    added goto

--- .local/environments/work.sh
+++ .local/environments/work.sh
@@ -9,13 +9,17 @@ export MTLS_CLIENT_CERT_FILE_PATH="/Users/jbeightol/jbeightol.staff.digitalocean
 [[ -f "${HOME}/.vauth" ]] && source "${HOME}/.vauth"
 
 alias pb='pinboard'
-alias orca0='evans -r -t repl'
-alias orca2='evans -r --tls --cert ~/jbeightol.staff.digitalocean.com.crt --certkey ~/jbeightol.staff.digitalocean.com.key --servername placement-services.internal.digitalocean.com repl'
-alias explain='go run "$HOME/Workspace/orca2/cmd/explainer/"'
+alias orca2='evans -r --tls --cert ~/jbeightol.staff.digitalocean.com.crt --certkey ~/jbeightol.staff.digitalocean.com.key repl'
+alias orca2-metal='orca2 --servername placement-services.internal.digitalocean.com'
+alias orca2-stage='orca2 --servername orca2-stage2.internal.digitalocean.com'
+alias explain='go run "$HOME/Workspace/cthulhu/docode/src/do/teams/compute/orca2/cmd/explainer/"'
 alias mine='jira list -n mine'
 alias todo='jira list -l 10 -n todo -p Orca'
 alias j='jira'
 alias flipperctl='docker run --rm docker.internal.digitalocean.com/compute/flipperctl'
+
+alias dote-clean-stage2='dote list on stage2 | cut -d \  -f 1 | tail -n +2 | xargs -I {} dote destroy {} on stage2'
+alias dote-clean-production='dote list on production | cut -d \  -f 1 | tail -n +2 | xargs -I {} dote destroy {} on production'
 
 function vauth() {
     local date=$(date +%s)
