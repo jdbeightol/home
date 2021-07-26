f3c1d6eaa9257d94eb0d18ca25517b8ab9a341da
Jeremy Beightol <jbeightol@digitalocean.com>
2021-07-26 13:00:37 -0400

    added pull alias for work

--- .local/environments/work.sh
+++ .local/environments/work.sh
@@ -23,6 +23,7 @@ echo; echo in-progress epic stories; line;
 jira list -n epics-inprogress -p Orca -t listissueonly | xargs -I{} jira epic ls {} -t list'
 alias j='jira'
 alias flipperctl='docker run --rm docker.internal.digitalocean.com/compute/flipperctl'
+alias pull='git fetch; git gc --prune=now; git pull'
 
 alias dote-clean-stage2='dote list on stage2 | cut -d \  -f 1 | tail -n +2 | xargs -I {} dote destroy {} on stage2'
 alias dote-clean-production='dote list on production | cut -d \  -f 1 | tail -n +2 | xargs -I {} dote destroy {} on production'
