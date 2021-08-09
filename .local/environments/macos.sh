147f8bae0422d15f5f2212968ad260da19da9feb
Jeremy Beightol <jdbeightol@gmail.com>
2021-08-09 19:38:12 -0400

    added dnsflush alias, because wtf

--- .local/environments/macos.sh
+++ .local/environments/macos.sh
@@ -14,6 +14,8 @@ alias find='find .'
 # Aliases of linux equivalent commands.
 alias lsblk='diskutil list'
 
+alias dnsflush='sudo killall -HUP mDNSResponder"
+
 # Source the git prompt function and add it into the PS1 if it exists.
 if [ -f "/Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-prompt.sh" ]; then
     source /Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-prompt.sh
