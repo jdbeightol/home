f21601fa8e9df1f8ad9dc64024179ed3a9611408
Jeremy Beightol <jbeightol@digitalocean.com>
2021-08-18 13:24:10 -0400

    work changes

--- .local/environments/work.sh
+++ .local/environments/work.sh
@@ -9,10 +9,10 @@ export MTLS_CLIENT_CERT_FILE_PATH="/Users/jbeightol/.local/ssl/jbeightol.staff.d
 export WORKSPACE_DIR="${CTHULHU_DIR}"
 
 alias pb='pinboard'
-alias orca2='evans -r --tls --cert ~/jbeightol.staff.digitalocean.com.crt --certkey ~/jbeightol.staff.digitalocean.com.key repl'
-alias orca2-metal='orca2 --servername placement-services.internal.digitalocean.com'
-alias orca2-stage='orca2 --servername orca2-stage2.internal.digitalocean.com'
-alias explain='go run "$HOME/Workspace/cthulhu/docode/src/do/teams/compute/orca2/cmd/explainer/"'
+alias doevans="evans -r --tls --cert '${MTLS_CLIENT_CERT_FILE}' --certkey '${MTLS_CLIENT_KEY_FILE}'"
+alias orca2='orca2-prod'
+alias orca2-prod='doevans --servername orca2.internal.digitalocean.com'
+alias orca2-stage2='doevans --servername orca2-stage2.internal.digitalocean.com'
 alias mine='jira list -n mine'
 alias todo='
 echo backlog; line
@@ -23,7 +23,6 @@ echo; echo in-progress epic stories; line;
 jira list -n epics-inprogress -p Orca -t listissueonly | xargs -I{} jira epic ls {} -t list'
 alias j='jira'
 alias flipperctl='docker run --rm docker.internal.digitalocean.com/compute/flipperctl'
-alias pull='git fetch; git gc --prune=now; git pull'
 
 alias dote-clean-stage2='dote list on stage2 | cut -d \  -f 1 | tail -n +2 | xargs -I {} dote destroy {} on stage2'
 alias dote-clean-production='dote list on production | cut -d \  -f 1 | tail -n +2 | xargs -I {} dote destroy {} on production'
