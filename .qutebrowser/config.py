ffa8afebf1423a3f646be166eb95052ddc92ae95
Jeremy Beightol <jbeightol@digitalocean.com>
2021-09-24 16:45:27 -0400

    updated qutebrowser config

--- .qutebrowser/config.py
+++ .qutebrowser/config.py
@@ -1,7 +1,7 @@
 # these custom bindings achieve the following:
 # - ensure that we leave any text boxes when running mode-leave for
-#   predictability
-# - to help with scrolling
+#   predictability when typing
+# - to help with scrolling speed
 # - switch J and K tab direction to be "left/right" rather than "up/down"
 c.bindings.commands = {
   "command": {},
@@ -25,12 +25,32 @@ c.input.insert_mode.auto_leave = False
 c.input.insert_mode.auto_load = False
 c.input.insert_mode.plugins = False
 
+# this one prevents sites from interrupting typing while the page is still loading
+c.input.insert_mode.leave_on_load = False
+
+# this prevents accidental insertion when in normal mode
+c.input.forward_unbound_keys = "none"
+
 # load tabs in the foreground; we can use ;r for background loading
 c.tabs.background = False
+
+# make it so the last used input mode is restored when switching tabs
+c.tabs.mode_on_change = "restore"
+
+# disable favicons in tabs because they're hard to see anyways
+c.tabs.favicons.show = "never"
 
 # try to make scrolling more visually tolerable since we changed scrolling 
 # distances above
 c.scrolling.smooth = True
+
+# change how downloads display and are automatically removed
+c.downloads.position = "bottom"
+c.downloads.remove_finished = 5000
+c.downloads.location.suggestion = "both"
+
+# disable notifications globally because they're annoying
+c.content.notifications.enabled = False
 
 # load autoconfig here to ensure local config overrides shared config --e.g.,
 # to use leave-mode rather than mode-leave on v2.0+
