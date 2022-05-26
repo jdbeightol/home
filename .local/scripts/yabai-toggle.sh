e1f9fd1d6598ee7837d0fba4287aa3f497f6e109
Jeremy Beightol <jbeightol@digitalocean.com>
2022-05-26 14:33:52 -0400

    tweak

--- .local/scripts/yabai-toggle.sh
+++ .local/scripts/yabai-toggle.sh
@@ -4,10 +4,10 @@ TYPE=$(yabai -m query --spaces --space | jq --raw-output .type)
 
 case "${TYPE}" in
     bsp)
-        WINS=($(yabai -m query --windows --space | jq '.[].id'))
+        WIN_IDS=($(yabai -m query --windows --space | jq '.[].id'))
         yabai -m space --layout float
-        for x in "${WINS[@]}"; do 
-            yabai -m window $x --grid 16:4:1:1:2:14
+        for ID in "${WIN_IDS[@]}"; do 
+            yabai -m window "$ID" --grid 16:4:1:1:2:14
         done
     ;;
     float)
