aec6b8bf0b5c3e9e6a50d5e5e7f0618b99cb00aa
Jeremy Beightol <jbeightol@digitalocean.com>
2022-05-26 16:01:03 -0400

    window spread for float

--- .local/scripts/yabai-toggle.sh
+++ .local/scripts/yabai-toggle.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
+WIN_WIDTH=4
+WIN_HEIGHT=7
+
+function window_spread() {
+    local win_ids=("$@")
+    local i=2
+    local j=1
+    for ID in "${WIN_IDS[@]}"; do
+        yabai -m window "$ID" --grid 16:16:$i:$j:$WIN_WIDTH:$WIN_HEIGHT
+        i=$((i+WIN_WIDTH))
+        if [[ $i -gt $((16 - WIN_WIDTH)) ]]; then
+            i=0
+            j=$((j + WIN_HEIGHT))
+        fi
+        if [[ $j -gt $((16 - WIN_HEIGHT - 1)) ]]; then
+            i=2
+            j=1
+        fi
+    done
+}
+
 TYPE=$(yabai -m query --spaces --space | jq --raw-output .type)
 
 case "${TYPE}" in
     bsp)
         WIN_IDS=($(yabai -m query --windows --space | jq '.[].id'))
         yabai -m space --layout float
-        for ID in "${WIN_IDS[@]}"; do 
-            yabai -m window "$ID" --grid 16:4:1:1:2:14
-        done
+        window_spread "${WIN_IDS}"
     ;;
     float)
         yabai -m space --layout stack
