a0bef0bb25c7e475f1fd3fb2e350a00b356c1026
Jeremy Beightol <jdbeightol@gmail.com>
2022-12-28 16:27:51 -0500

    added test for repo init

--- .local/scripts/git-mk.sh
+++ .local/scripts/git-mk.sh
@@ -1,11 +1,15 @@
-#!/bin/bash
+#!/bin/bash 
 
 repo=${1:?missing repo name}
 readonly repo
 
-if [[ -n "$(git remote get-url origin)" ]]; then
-    1>&2 echo 'error: remote origin already exists'
+if [[ -z "$(git rev-parse HEAD)" ]]; then
     exit 1
+fi
+
+if [[ -n "$(git remote get-url origin 2>/dev/null)" ]]; then
+    1>&2 echo 'error: remote origin already exists'
+    exit 2
 fi
 
 git remote add origin "${GITADDR:?GITADDR not set}/${repo}"
