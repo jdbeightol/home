76f43b08b1f75a18e66058a0776779d48322a765
Jeremy Beightol <jbeightol@digitalocean.com>
2023-01-05 13:07:38 -0500

    added nanoleaf scripts

---
+++ .local/scripts/nanoleaf-sparrows.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import yaml
+from nanoleafapi import Nanoleaf
+
+config_file = "/Users/jbeightol/.nanoleaf.yml"
+
+with open(config_file) as f:
+    cfg = yaml.safe_load(f)
+
+nl=Nanoleaf(ip=cfg['ip'], auth_token=cfg['token'] if 'token' in cfg else None)
+nl.set_effect("Sparrows")
