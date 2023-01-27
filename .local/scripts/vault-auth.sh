2714a489147099ba36202fa1f61f56c0f8bd0230
Jeremy Beightol <jbeightol@digitalocean.com>
2023-01-27 18:57:19 -0500

    never nest tweak to vauth

--- .local/scripts/vault-auth.sh
+++ .local/scripts/vault-auth.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 VAULT_CACHE_FILE="${HOME}/.vault-auth"
 
 [[ -f "${VAULT_CACHE_FILE}" ]] && source "${VAULT_CACHE_FILE}"
@@ -10,25 +9,26 @@ function vault::auth() {
     local expire=72000
 
     # Check if the expiration is either empty or if we are after the expiration period
-    if [[ -z "${VAULT_EXPIRE}" ]] || [[ ${date} -ge ${VAULT_EXPIRE} ]]; then
-        local token="$(vault login -method=oidc -path=oidc-okta -token-only username=$USER)"
-
-        if [[ -z "${token}" ]]; then
-            1>&2 echo "returning: no token available"
-            # We don't want to set or write anything if there was an error
-            return
-        fi
-
-        1>&2 echo "success: setting token and updating cache"
-
-        export VAULT_TOKEN="${token}"
-        export VAULT_EXPIRE="$((date + expire))"
-
-        # Cache our new vault token and expiration date as a environment variables
-        vault::cache
-    else
+    if [[ -n "${VAULT_EXPIRE}" ]] && [[ ${date} -lt ${VAULT_EXPIRE} ]]; then
         1>&2 echo "using cached credentials..."
+        return
     fi
+
+    local token="$(vault login -method=oidc -path=oidc-okta -token-only username=$USER)"
+
+    if [[ -z "${token}" ]]; then
+        1>&2 echo "returning: no token available"
+        # We don't want to set or write anything if there was an error
+        return
+    fi
+
+    1>&2 echo "success: setting token and updating cache"
+
+    export VAULT_TOKEN="${token}"
+    export VAULT_EXPIRE="$((date + expire))"
+
+    # Cache our new vault token and expiration date as a environment variables
+    vault::cache
 }
 
 function vault::cache() {
@@ -41,4 +41,3 @@ EOF
 
 alias vault-auth="vault::auth"
 alias vauth="vault::auth"
-
