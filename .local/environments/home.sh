c09130556002cc5c449244e3223f34ab28018ff5
Jeremy Beightol <jdbeightol@gmail.com>
2023-03-06 05:55:34 -0500

    added list alias to home

--- .local/environments/home.sh
+++ .local/environments/home.sh
@@ -19,3 +19,11 @@ export NOMAD_ADDR="http://mimas.saturn.sol:4646"
 export CONSUL_HTTP_ADDR="http://mimas.saturn.sol:8500"
 
 export VAULT_ADDR="http://vault.uranus.sol/"
+
+alias list="
+dig @10.12.13.2 pluto.sol axfr |
+    \grep -E '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' |
+    \grep -v ';' | sed -e 's/\(^[0-9A-Za-z.\-]*\.pluto\.sol\)\..*$/\1/' |
+    sort |
+    uniq
+"
