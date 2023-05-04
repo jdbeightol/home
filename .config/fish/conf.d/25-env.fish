f38aaf7de7eff44d6613cdc6cd9821a47e5538b4
Jeremy Beightol <jdbeightol@gmail.com>
2023-05-04 01:22:11 -0400

    fish: updated hierarchical environment
    This work updates the hierarchical environment loading to behave similar
    to bash.  Also renamed macos -> darwin to match `uname`.

--- .config/fish/conf.d/25-env.fish
+++ .config/fish/conf.d/25-env.fish
@@ -1,11 +1,21 @@
 #!/usr/bin/env fish
 
 function env::require
-    source $__fish_environment_dir/$argv[1].fish
+    set -f filename $__fish_environment_dir/$argv[1].fish
+    if test -f $filename
+        source $filename
+        set -ag __fish_profile_stack $argv[1]
+        return 0
+    end
+    return 1
 end
 
 set -g __fish_environment_dir $__fish_config_dir/environments
 
-if set -q __fish_environment
-    env::require $__fish_environment
+if not set -q __fish_environment || not env::require $__fish_environment
+    if not env::require (hostname -s | string lower)
+        env::require (uname | string lower)
+    end
 end
+
+set fish_greeting "Configuration hierarchy: config" "> "{$__fish_profile_stack}
