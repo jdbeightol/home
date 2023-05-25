7860a3499c9c95ca7384c264db9153d7a46178d0
Jeremy Beightol <jbeightol@digitalocean.com>
2023-05-25 14:24:30 -0400

    env: fixed wrong cargo path

--- .config/fish/environments/work.fish
+++ .config/fish/environments/work.fish
@@ -34,4 +34,4 @@ set -gx VAULT_ADDR 'https://vault-api.internal.digitalocean.com:8200'
 set -gx MTLS_CLIENT_KEY_FILE_PATH $HOME/.local/ssl/jbeightol.staff.digitalocean.com.key
 set -gx MTLS_CLIENT_CERT_FILE_PATH $HOME/.local/ssl/jbeightol.staff.digitalocean.com.crt
 
-fish_add_path -ga $HOME/.local/cargo/bin
+fish_add_path -ga $HOME/.cargo/bin
