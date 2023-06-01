d9cecf2f8b95aa2f514b22228309237ea8e21884
Jeremy Beightol <jdbeightol@gmail.com>
2023-06-01 03:28:42 -0400

    starship: disabled transient prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -32,5 +32,3 @@ set fish_greeting "$(set_color -oi white)Configuration hierarchy: $(set_color -o
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
-enable_transience
